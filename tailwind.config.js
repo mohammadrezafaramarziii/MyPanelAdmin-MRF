3ac7da1583d012d1c905125cd37ffe3fda76b940
MohammadReza-MRF <mf575583@gmail.com>
2023-02-09 12:37:22 -0400

    Upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/*.{html}",
+    "./index.html"],
+  theme: {
+    fontFamily: {
+      body: ['Bakh'],
+      display: ['Bakh'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
